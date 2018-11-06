9a8914443b157b3818d3cffa55d658f3a34080d9
Nicolás López <nicolaslopezj@me.com>
2018-11-06 13:53:43 -0300

    improve hot module reloading Fixes hooks integration

--- web/src/index.js
+++ web/src/index.js
@@ -1,27 +1,15 @@
 import 'babel-polyfill'
-import 'react-hot-loader/patch'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './styles'
-import {AppContainer} from 'react-hot-loader'
 
-ReactDOM.render(
-  <AppContainer>
-    <App />
-  </AppContainer>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // Hot Module Replacement API
 if (module.hot) {
   module.hot.accept('./App', () => {
     const NextApp = require('./App').default
-    ReactDOM.render(
-      <AppContainer>
-        <NextApp />
-      </AppContainer>,
-      document.getElementById('root')
-    )
+    ReactDOM.render(<NextApp />, document.getElementById('root'))
   })
 }
